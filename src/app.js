c2fa28aa9f3b287ca956c814c441e5587a9a7c47
Daniel <garciaantoniodaniel11@gmail.com>
2019-12-06 18:49:47 +0100

    Project Finish

--- src/app.js
+++ src/app.js
@@ -123,6 +123,21 @@ App = {
     window.location.reload()
   },
 
+  createTask: async () => {
+  	App.setLoading(true)
+  	const content = $('#newTask').val()
+
+  	await App.todoList.createTask(content)
+  	window.location.reload()
+  },
+
+  toggleCompleted: async (e)=> {
+  	App.setLoading(true)
+  	const taskId = e.target.name
+  	await App.todoList.toggleCompleted(taskId)
+  	window.location.reload()
+  },
+
   setLoading: (boolean) => {
     App.loading = boolean
     const loader = $('#loader')
